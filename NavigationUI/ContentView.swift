64e3b60671f65e83d1f3dd042ddd47720528ccf0
walkinglele <leleberjalan@gmail.com>
2021-01-29 10:25:50 +0700

    Initial Commit

---
+++ NavigationUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationUI
+//
+//  Created by Dzaky Saputra on 29/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
